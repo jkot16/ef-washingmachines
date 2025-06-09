eeeca6eec204d54aa5f686222d4110225558f0d4
Jakub Kot <kotjakub16@gmail.com>
2025-06-09 16:46:58 +0200

    add contracts and services structures

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Test2S30964JakubKot.Data;
+using Test2S30964JakubKot.Services;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IPurchaseService, PurchaseService>();
+builder.Services.AddScoped<IWashingMachineService, WashingMachineService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
